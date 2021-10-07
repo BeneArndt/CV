97f2f7890ba0b51467e75f8a9c1e676038533f54
Benedikt Arndt <bene.gd.go@gmail.com>
2021-10-07 15:38:21 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close'),
-      navToggleIcon = document.getElementById('nav-toggle-icon'),
-      skillContent = document.getElementsByClassName('skills_content'),
-      skillsHeader = document.querySelectorAll('.skills_header')
+      navToggleIcon = document.getElementById('nav-toggle-icon')
 /*show menu*/
   navToggle.addEventListener('click',() =>{
     toggleMenu();
@@ -24,6 +22,8 @@ function toggleMenu(){
   }
 }
 /* skills menu */
+const skillContent = document.getElementsByClassName('skills_content'),
+skillsHeader = document.querySelectorAll('.skills_header')
 function toggleSkills(){
 
   let itemclass = this.parentNode.className
@@ -38,3 +38,35 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
   el.addEventListener('click', toggleSkills)
 })
+
+/* qualification menu */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabsContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification_active')
+    })
+    target.classList.add('qualification_active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification_active')
+    })
+    tab.classList.add('qualification_active')
+  })
+})
+/*swiper JS */
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
